8792a78b8e04b912d07675e59176a761dc5c67c4
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-11-03 21:35:50 +0200

    lab 16+17

---
+++ src/main/java/com/example/codefellowship/Repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.codefellowship.Repo;
+
+import com.example.codefellowship.Model.ApplicationUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepo extends CrudRepository<ApplicationUser,Integer> {
+    ApplicationUser findByUsername(String username);
+
+}
